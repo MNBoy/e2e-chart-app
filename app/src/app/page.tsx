72f9cd0a6f4f4d96af0a018264cf2aca9f5a2c07
Moein Salari <moeinsalar7@gmail.com>
2024-05-23 11:46:54 +0330

    feat(chat): add base of chat page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <main>Hello World</main>;
+  return (
+    <main className='flex h-[calc(100dvh)] flex-col items-center justify-center p-4 md:px-24 py-32 gap-4'>
+      <div className='flex justify-between max-w-5xl w-full items-center'>
+        <Link href='#' className='text-4xl font-bold text-gradient'>
+          Chat Application
+        </Link>
+      </div>
+
+      <div className='z-10 border rounded-lg max-w-5xl w-full h-full text-sm lg:flex'>
+        {/* <ChatLayout defaultLayout={defaultLayout} navCollapsedSize={8} /> */}
+      </div>
+    </main>
+  );
 }
